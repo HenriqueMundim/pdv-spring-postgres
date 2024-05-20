dc82618388687297ba2b830a24348c1232db6df3
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-20 14:42:08 -0300

    feat: add findById route

--- src/main/java/com/personalprojects/pdv/app/controllers/UserController.java
+++ src/main/java/com/personalprojects/pdv/app/controllers/UserController.java
@@ -1,7 +1,21 @@
 package com.personalprojects.pdv.app.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.personalprojects.pdv.domain.entities.User;
+import com.personalprojects.pdv.domain.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping(value = "/users")
 public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
