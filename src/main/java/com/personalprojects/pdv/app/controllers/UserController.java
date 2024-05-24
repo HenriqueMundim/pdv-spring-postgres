dd28f4dd8596a92ff4af4e37786f9a3aaad1ac52
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-24 17:41:45 -0300

    feat: docs in enpoints Implement documentation for all endpoint

--- src/main/java/com/personalprojects/pdv/app/controllers/UserController.java
+++ src/main/java/com/personalprojects/pdv/app/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.personalprojects.pdv.app.controllers;
 
 import com.personalprojects.pdv.domain.errors.StandardException;
 import com.personalprojects.pdv.domain.services.UserService;
+import com.personalprojects.pdv.infra.dto.CreateUser;
 import com.personalprojects.pdv.infra.dto.UserDto;
+import com.personalprojects.pdv.infra.mappers.UserMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -21,6 +23,26 @@ public class UserController {
     @Autowired
     public UserService userService;
 
+    @Operation(
+            description = "This endpoint returns a user with the specified ID",
+            summary = "Get a user by ID",
+            responses = {
+                    @ApiResponse(
+                            description = "User with this ID are found",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User with this ID not found",
+                            responseCode = "404",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = StandardException.class)
+                                    )
+                            }
+                    )
+
+            }
+    )
     @GetMapping(
             value = "/{id}",
             produces = {"application/json", "application/xml", "application/x-yaml"}
@@ -40,7 +62,7 @@ public class UserController {
                     ),
                     @ApiResponse(
                             description = "User with this email already exists",
-                            responseCode = "404",
+                            responseCode = "400",
                             content = {
                                     @Content(
                                             schema = @Schema(implementation = StandardException.class)
@@ -53,15 +75,36 @@ public class UserController {
             consumes = {"application/json", "application/xml", "application/x-yaml"},
             produces = {"application/json", "application/xml", "application/x-yaml"}
     )
-    public ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
-        UserDto newUser = userService.create(userDto);
+    public ResponseEntity<UserDto> create(@RequestBody CreateUser data) {
+        UserDto newUser = userService.create(UserMapper.createUserToDto(data));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
+    @Operation(
+            description = "This endpoint delete one user in our system",
+            summary = "Delete a user",
+            responses = {
+                    @ApiResponse(
+                            description = "User deleted successful",
+                            responseCode = "204",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            description = "User with this ID not found",
+                            responseCode = "404",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = StandardException.class)
+                                    )
+                            }
+                    )
+            }
+    )
     @DeleteMapping(
             value = "/{id}",
-            consumes = {"application/json", "application/xml", "application/x-yaml"}
+            consumes = {"application/json", "application/xml", "application/x-yaml"},
+            produces = {"application/json", "application/xml", "application/x-yaml"}
     )
     public ResponseEntity<?> delete(@PathVariable String id) {
         userService.delete(id);
