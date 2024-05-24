23f10f59d6eab9530e576b6e5c1a8b31f7d1c234
Henrique Matos <henriquemundimmatos@gmail.com>
2024-05-24 18:19:52 -0300

    feat: add service layer implementation Create product mapper and implement all service layer

--- src/main/java/com/personalprojects/pdv/domain/services/ProductService.java
+++ src/main/java/com/personalprojects/pdv/domain/services/ProductService.java
@@ -1,7 +1,23 @@
 package com.personalprojects.pdv.domain.services;
 
+import com.personalprojects.pdv.domain.entities.Product;
+import com.personalprojects.pdv.infra.dto.ProductDto;
+import com.personalprojects.pdv.infra.mappers.ProductMapper;
+import com.personalprojects.pdv.infra.repositories.ProductRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+
+    @Autowired
+    private ProductRespository repository;
+
+
+
+    public ProductDto create(ProductDto productDto) {
+        Product newProduct = repository.create(ProductMapper.toEntity(productDto));
+
+        return ProductMapper.toDto(newProduct);
+    }
 }
--- src/main/java/com/personalprojects/pdv/infra/dto/ProductDto.java
+++ src/main/java/com/personalprojects/pdv/infra/dto/ProductDto.java
@@ -1,4 +1,22 @@
 package com.personalprojects.pdv.infra.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class ProductDto {
+
+    private String id;
+
+    @EqualsAndHashCode.Exclude
+    private String description;
+
+    @EqualsAndHashCode.Exclude
+    private Double price;
+
+    @EqualsAndHashCode.Exclude
+    private String imageUrl;
 }
