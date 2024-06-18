9d72f82430924b821d891de5e0c7bbb0fcb5bfe3
Henrique Matos <henriquemundimmatos@gmail.com>
2024-06-18 10:56:13 -0300

    fix: Exception in spring security filter Exception handler created to resolve exeptions throws in security filter, now have a custom message response

--- src/main/java/com/personalprojects/pdv/domain/errors/StandardException.java
+++ src/main/java/com/personalprojects/pdv/domain/errors/StandardException.java
@@ -15,4 +15,11 @@ public class StandardException {
     private Instant date;
     private String path;
     private String cause;
+
+    @Override
+    public String toString() {
+        return "\"date\": " + this.getDate() + "," + "\n"
+                + " \"path\": " + this.getPath() + "," + "\n"
+                + " \"message\": " + this.getCause();
+    }
 }
--- src/main/java/com/personalprojects/pdv/infra/security/SecurityFilter.java
+++ src/main/java/com/personalprojects/pdv/infra/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package com.personalprojects.pdv.infra.security;
 
+import com.personalprojects.pdv.domain.errors.StandardException;
 import com.personalprojects.pdv.domain.errors.TokenVerificationException;
 import com.personalprojects.pdv.domain.services.TokenService;
 import com.personalprojects.pdv.infra.repositories.UserRepository;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Instant;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -26,21 +29,37 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException  {
-        var token = recoverToken(request);
-        if (token != null) {
-            var username = service.validateToken(token);
-            var user = repository.findByUsername(username);
+        try {
+            var token = recoverToken(request);
+            if (token != null) {
+                var username = service.validateToken(token);
+                var user = repository.findByUsername(username);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (Exception exception) {
+            handleFilterException(exception, request, response);
         }
-
-        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
         if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
+    }
+
+    private void handleFilterException(Exception exception, HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setStatus(403);
+        response.setContentType("application/json");
+
+        PrintWriter out = response.getWriter();
+        String errorResponse = new StandardException(Instant.now(), request.getRequestURI(), exception.getMessage()).toString();
+        out.print("{" + errorResponse + "}");
+
+        out.flush();
     }
 }
